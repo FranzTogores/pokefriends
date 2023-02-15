53c86a384e65def6de30bb99f2fc718de8e2c210
BokkenKun <fransantogores@gmail.com>
2023-02-15 13:19:10 +0100

    Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,36 +6,27 @@ import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            pokemons: [],
-            searchfield: ''
-        }
-    }
-
-    componentDidMount() {
+function App() {
+    const [pokemons, setPokemons] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    useEffect( () => {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ pokemons: users }));
-    }
+        .then(response => response.json())
+        .then(users => {setPokemons(users)});
+    }, [])
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
-
-    render() {
-        const { pokemons, searchfield } = this.state;
-        const filteredPokemons = pokemons.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        const filteredPokemons = pokemons.filter(pokemon => {
+            return pokemon.name.toLowerCase().includes(searchfield.toLowerCase());
         })
         return !pokemons.length ?
             <h1>Loading</h1> :
             (
                 <div className='tc'>
                     <h1 className='f1'>PokéFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList pokemons={filteredPokemons} />
@@ -45,6 +36,6 @@ class App extends Component {
             );
     }
 
-}
+
 
 export default App;
